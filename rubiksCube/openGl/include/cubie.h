ebe04762439dacfbad453e6f976acd97c32a69a4
Michael Griese <mjgriese@email.wm.edu>
2020-12-12 00:06:33 -0500

    Revert "Seperating header files from implementation"
    This reverts commit 4ed561e78cde4c44d1bc74d364218eefbce72322.

--- rubiksCube/openGl/include/cubie.h
+++ rubiksCube/openGl/include/cubie.h
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <glm/ext.hpp>
 #include <glm/gtx/string_cast.hpp>
-#include <shader.h>
 
 class Cubie {
 	
@@ -17,37 +16,63 @@ private:
 	glm::mat4 modelMatrix = glm::mat4(1.0f);
 
 public:
+	Cubie() {
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+		this->localPosition = glm::vec3(x, y, z);
+	}
 	//Constructor
 	Cubie(glm::vec3 localMatrix, float x, float y, float z) {
-		this->localPosition = glm::vec3(x, y, z);
+		this -> localPosition = glm::vec3(x, y, z);
 		this->x = x;
 		this->y = y;
 		this->z = z;
 		modelMatrix = glm::translate(modelMatrix, localPosition);
-	} 
+	}
 
-	void draw(Shader ourShader);
+	void draw(Shader ourShader) {
+		ourShader.setMat4("model", modelMatrix);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 
-	float getX() {return x;}
+	float getX() {
+		return x;
+	}
 
-	float getY() {return y;}
+	float getY() {
+		return y;
+	}
 
-	float getZ() {return z;}
+	float getZ() {
+		return z;
+	}
 
-	void rotate(float angle, glm::vec3 rotationAxis);
-	
-	void reset();
+	void rotate(float angle, glm::vec3 rotationAxis) {
+		modelMatrix = glm::rotate(modelMatrix, glm::radians(angle), rotationAxis);
+		
+	}
+
+	void reset() {
+		modelMatrix = glm::mat4(1.0f);
+		modelMatrix = glm::translate(modelMatrix, localPosition);
+	}
 
 	void setPos(glm::vec3 newPos) {
 		this->x = newPos[0];
 		this->y = newPos[1];
 		this->z = newPos[2];
+
 		localPosition = newPos;
 	}
-
-	void setModel(glm::mat4 newModel) {modelMatrix = newModel;}
-
-	glm::mat4 getModel() {return modelMatrix;}
 };
+
+
+
+
+
+
+
+
 
 #endif
