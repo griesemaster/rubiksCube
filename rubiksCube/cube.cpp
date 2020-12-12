e3b0166e59d4df689765fe0817453ef5feeec140
Michael Griese <mjgriese@email.wm.edu>
2020-12-12 00:21:51 -0500

    Sucessfully sperated the cube and cubie implementation from their repcetive header files to improve coding practice.

--- rubiksCube/cube.cpp
+++ rubiksCube/cube.cpp
@@ -1,4 +1,4 @@
-#include "rubiksCube.h"
+#include "cube.h"
 #include <glm/glm.hpp>
 #include<glm/gtc/matrix_transform.hpp>
 #include<cubie.h>
@@ -8,40 +8,61 @@
 #include <GLFW/glfw3.h>
 
 
-	void Cube::generateCubies() {
-		for (float x = 0; x < cubeDimension; x++) {
-			for (float y = 0; y < cubeDimension; y++) {
-				for (float z = 0; z < cubeDimension; z++) {
-					cubieList.emplace_back(Cubie(glm::vec3(x, y, z), x, y, z));
-				}
+void Cube::generateCubies() {
+	for (float x = 0; x < cubeDimension; x++) {
+		for (float y = 0; y < cubeDimension; y++) {
+			for (float z = 0; z < cubeDimension; z++) {
+				cubieList.emplace_back(Cubie(glm::vec3(x, y, z), x, y, z));
 			}
 		}
-		std::cout << "made cubies" << std::endl;
 	}
+	std::cout << "made cubies" << std::endl;
+}
 
-	void Cube::draw(Shader ourShader) {
+void Cube::draw(Shader ourShader) {
+	for (Cubie currentCubie : cubieList) {
+		currentCubie.draw(ourShader);
+	}
+}
+
+void Cube::reset() {
+
+}
+
+bool Cube::checkValidCommand() {
+	if (lastCommandTime + commandDelay < glfwGetTime()) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+//Z Face Rotations
+void Cube::rotateZzeroClockwise(bool automated) {
+	if (Cube::checkValidCommand()) {
 		for (Cubie currentCubie : cubieList) {
-			currentCubie.draw(ourShader);
-		}
-	}
-
-	void Cube::reset() {
-
-	}
-
-
-	//Z Face Rotations
-	void Cube::rotateZzeroClockwise(bool automated) {
-		if (checkValidCommand()) {
-			for (Cubie currentCubie : cubieList) {
-				if (currentCubie.getZ() == 1) {
-					glm::mat4 currentRotation = currentCubie.getModel();
-					glm::mat4 newRotation = glm::rotate(currentRotation, glm::radians(90.0f), glm::vec3(0, 0, 1));
-					currentCubie.setModel(newRotation);
-				}
+			if (currentCubie.getZ() == 1) {
+				glm::mat4 currentRotation = currentCubie.getModel();
+				glm::mat4 newRotation = glm::rotate(currentRotation, glm::radians(90.0f), glm::vec3(0, 0, 1));
+				currentCubie.setModel(newRotation);
 			}
-			lastCommandTime = glfwGetTime();
 		}
+		lastCommandTime = glfwGetTime();
 	}
+}
+
+void Cube::rotateZzeroCounterwise(bool automated) {
+	if (Cube::checkValidCommand()) {
+		for (Cubie currentCubie : cubieList) {
+			if (currentCubie.getZ() == 1) {
+				glm::mat4 currentRotation = currentCubie.getModel();
+				glm::mat4 newRotation = glm::rotate(currentRotation, glm::radians(-90.0f), glm::vec3(0, 0, 1));
+				currentCubie.setModel(newRotation);
+			}
+		}
+		lastCommandTime = glfwGetTime();
+	}
+}
 
 
